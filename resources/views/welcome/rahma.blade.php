6e393280cdb2b8288f228969be77396a799f2701
rahmaxyn <kunnisofarahma06@gmail.com>
2025-09-10 12:59:24 +0700

    minggu2

---
+++ resources/views/welcome/rahma.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang</title>
+    <style>
+        body {
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+            background: linear-gradient(135deg, #7ca3edff, #9b5dddff, #e97965ff);
+            color: #fff;
+        }
+        .container {
+            background: rgba(255, 255, 255, 0.1);
+            padding: 30px 50px;
+            border-radius: 12px;
+            text-align: center;
+            box-shadow: 0 6px 20px rgba(0,0,0,0.25);
+        }
+        h1 {
+            font-size: 2.5rem;
+            margin-bottom: 10px;
+        }
+        p {
+            font-size: 1.1rem;
+            opacity: 0.9;
+        }
+        button {
+            margin-top: 20px;
+            background-color: #2e418aff; 
+            border: none;
+            color: white;
+            padding: 12px 24px;
+            border-radius: 8px;
+            font-size: 16px;
+            cursor: pointer;
+            transition: 0.3s;
+        }
+
+        button:hover {
+            background-color: #2869ffff;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1> Halo, Selamat Datang!</h1>
+        <button>Mulai Jelajahi</button>
+    </div>
+</body>
+</html>
